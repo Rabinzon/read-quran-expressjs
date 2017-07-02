6ae5cf4a3faf67483c2e8264dbeaa2b17b6dfeab
rabinzon <ildarrazin@gmail.com>
2017-07-02 16:08:49 +0300

    feat: add bookmark

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,10 +1,13 @@
 function init() {
     var $body = $('body');
+    var $window =  $(window);
 
     var selectors = {
         ayat: '.ayat',
         continue: '.js-сontinue-btn',
-        toTop: '.js-to-top-btn'
+        toTop: '.js-to-top-btn',
+        bookmark: '.js-to-bookmark-btn',
+        continueReading: '.js-continue-reading'
     };
 
     function scrollToAnchor(position) {
@@ -22,7 +25,7 @@ function init() {
         }
     }
 
-    function copy(e) {
+   /* function copy(e) {
         var text = window.getSelection().toString();
         var surah = '';
 
@@ -42,30 +45,47 @@ function init() {
             e.originalEvent.clipboardData.setData('text/plain', result + '\n' + surah);
             e.preventDefault();
         }
-    }
+    }*/
 
-    /*function redirect() {
+    function toBookmark() {
         var path = window.location.pathname;
         var savedPath = window.localStorage.getItem('readquran.ru/path');
         if (path != savedPath) {
-            window.location.pathname = savedPath;
+            window.location = savedPath;
         }
-    }*/
+    }
 
-    function savePosition() {
+    function getCurrentAyat() {
+        const scrollY = window.scrollY;
+        return $(selectors.ayat)
+            .toArray()
+            .reduce((acc, cur) =>
+                (scrollY + 80) - $(cur).offset().top > 0 ? cur : acc);
+    }
+
+    function setBookmark() {
         var path = window.location.pathname;
         if (!path) {
             return;
         }
-        window.localStorage.setItem('readquran.ru/position', document.body.scrollTop);
-        window.localStorage.setItem('readquran.ru/path', path);
+        const current = getCurrentAyat();
+        const hash = $(current).attr('id');
+        window.localStorage.setItem('readquran.ru/path', path + '#' + hash);
+    }
+
+    function showBookmarkLink() {
+        var path = window.location.pathname;
+        var savedPath = window.localStorage.getItem('readquran.ru/path');
+        if (path != savedPath) {
+            $(selectors.continueReading).show();
+        }
     }
 
     var prevScrollPos = false;
 
     function onScroll() {
         var scrollY  = window.scrollY;
-        if (scrollY > 130 && prevScrollPos >= window.scrollY) {
+        if (scrollY > 130 && (prevScrollPos >= scrollY)) {
             $body.addClass('to-top-button');
         } else {
             $body.removeClass('to-top-button');
@@ -75,9 +95,14 @@ function init() {
 
     // $(selectors.ayat).on('copy', copy);
     $(selectors.toTop).on('click', scrollToAnchor);
-    // $(selectors.continue).on('click', redirect);
-    $(window).on('scroll', onScroll);
+    $(selectors.bookmark).on('click', setBookmark);
+    $(selectors.continueReading).on('click', toBookmark);
+    showBookmarkLink();
+    $window.on('scroll', onScroll);
     scrollByHash();
+    $window.on('beforeunload', function() {
+        setBookmark();
+    })
 }
 
 $(document).ready(init);
