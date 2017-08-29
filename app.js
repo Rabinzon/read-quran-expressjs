42e13c462cf107291dec1a6da46dea0ef04d4613
rabinzon <ildarrazin@gmail.com>
2017-08-29 10:35:52 +0300

    feat: add gzip

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const compression = require('compression');
 
 require('./models/db');
 
@@ -25,6 +26,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(compression());
 
 app.use('/', index);
 app.use('/krac/', surah);
